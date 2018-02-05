80eb95a5bf543a35757e664ca26b5cdf2a93a2d2
VenusMeow <32319682+VenusMeow@users.noreply.github.com>
2018-02-05 09:31:42 -0500

    modify

--- VenusCircle.java
+++ VenusCircle.java
@@ -1,7 +1,14 @@
 package pa03;
 
+import java.awt.Color;
+
 public class VenusCircle extends CircleShape{
 
+  public VenusCircle(){
+    super();
+    this.color = new Color(0,0,0);
+  }
+  
   public static void main(String[] args){
     VenusCircle x = new VenusCircle();
     System.out.println("my circle is "+x);

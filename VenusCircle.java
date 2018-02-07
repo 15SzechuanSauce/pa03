205ca9ff3711127fb91e6b232e8743846e6da5a2
VenusMeow <32319682+VenusMeow@users.noreply.github.com>
2018-02-07 09:50:55 -0500

    modified something

--- VenusCircle.java
+++ VenusCircle.java
@@ -1,20 +1,36 @@
 package pa03;
 
-
 import java.awt.Color;
+import java.awt.Graphics;
 
 public class VenusCircle extends CircleShape{
 
   public VenusCircle(){
     super();
-    this.color = new Color(0,0,0);
+    this.color = new Color(217,242,255); //color: baby blues
   }
 
-  public static void main(String[] args){
-    VenusCircle x = new VenusCircle();
-    System.out.println("my circle is "+x);
-  }
-  public String toString(){
-    return "Venus's Circle: "+ super.toString();
+  public void keepOnBoard(){
+    if (this.x < this.radius) {
+      // it went off the left edge! do something!
+      this.vx = 0;
+      this.x = this.radius;
+
+    }else if (this.x > CircleShape.boardWidth-this.radius) {
+      // it went off the right edge! do something!
+      this.vx = 0;
+      this.x = CircleShape.boardWidth-this.radius;
+    }
+
+    if (this.y < this.radius){
+      // it went above the top edge!
+      this.vy = 0;
+      this.y = this.radius;
+
+    } else if (this.y > CircleShape.boardHeight-this.radius) {
+      // it went below the bottom edge!
+      this.vy = 0;
+      this.y = CircleShape.boardHeight-this.radius;
+    }
   }
 }

1e0c7c439c3c1168cf7bfad3262150961c9f8af7
Elaine <32910452+elaineyu1031@users.noreply.github.com>
2018-02-08 22:10:33 -0500

    change borderColor and add text in circle

--- xiaoyangCircle.java
+++ xiaoyangCircle.java
@@ -1,17 +1,36 @@
 package pa03;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.*;
+
+
 
 public class xiaoyangCircle extends CircleShape{
+  public Color borderColor;
+  public Color textColor;
 
   public xiaoyangCircle(){
     super();
-    this.color = new Color(200,0,0);
+    this.color = new Color(200,0,0); //color:red
+    this.borderColor=new Color(200,0,0);
   }
+  public void draw( Graphics g ) {
+    //super.draw(g);
+    int u = (int)(x-radius);
+    int v = (int)(y-radius);
+    int w = (int)(2*radius);
+    int h = w;
+    g.setColor( color );
+    g.fillOval( u,v,w,h );
+    g.setColor( borderColor);
+    g.drawOval(u,v,w,h );
+    Font helvetica = new Font("Helvetica", Font.PLAIN, 18);
+    g.setFont(helvetica);
+    this.textColor=new Color(0,0,0);
+    g.setColor(textColor);
+    g.drawString("YXY",(int)x,(int)y);
 
-  public static void main(String[] args) {
-    xiaoyangCircle x = new xiaoyangCircle();
-    System.out.println("my circle is "+x);
   }
 
   public String toString(){

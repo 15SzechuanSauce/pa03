c263714f62d94dfb8524861974f5b4877dd4c7f1
VenusMeow <32319682+VenusMeow@users.noreply.github.com>
2018-02-11 18:10:19 -0500

    modified drawingmodel and demo

--- DrawingModel.java
+++ DrawingModel.java
@@ -27,16 +27,11 @@ public class DrawingModel {
 	*/
 	private void init(){
 		CircleShape c;
-		for(int i=0; i< circles.length/5; i++){
+		for(int i=0; i< circles.length/4; i++){
 			// we go to circles.length/2 as we add 2 different kinds of circles
 			// each time through this loop.. with 5 classes you'll divide by 5
 			// WHEN YOU ADD ANOTHER SUBCLASS OF CIRCLESHAPE YOU NEED TO
 			// CREATE IT AND ADD IT TO MODEL
-			c = new CircleShape();
-			this.add(c);
-
-      c = new RadiusChangingCircle();
-      this.add(c);
 
 			c= new xiaoyangCircle();
 			this.add(c);
@@ -45,6 +40,9 @@ public class DrawingModel {
 			this.add(c);
 
 			c = new SandyCircle();
+			this.add(c);
+
+			c = new CircleShape();
 			this.add(c);
 		}
 	}

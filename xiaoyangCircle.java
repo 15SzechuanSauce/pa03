df600ac772e4a396d041a5f53d9fa6fbcc9f93dd
Elaine <32910452+elaineyu1031@users.noreply.github.com>
2018-02-12 12:51:56 -0500

    Change textColor

--- xiaoyangCircle.java
+++ xiaoyangCircle.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.*;
 
-
-
 public class xiaoyangCircle extends CircleShape{
   public Color borderColor;
   public Color textColor;
@@ -14,23 +12,43 @@ public class xiaoyangCircle extends CircleShape{
     super();
     this.color = new Color(200,0,0); //color:red
     this.borderColor=new Color(200,0,0);
+    this.textColor=new Color(0,0,0);
   }
   public void draw( Graphics g ) {
-    //super.draw(g);
-    int u = (int)(x-radius);
-    int v = (int)(y-radius);
-    int w = (int)(2*radius);
-    int h = w;
-    g.setColor( color );
-    g.fillOval( u,v,w,h );
-    g.setColor( borderColor);
-    g.drawOval(u,v,w,h );
-    Font helvetica = new Font("Helvetica", Font.PLAIN, 18);
+    super.draw(g);
+    Font helvetica = new Font("Helvetica", Font.BOLD, 18);
     g.setFont(helvetica);
-    this.textColor=new Color(0,0,0);
     g.setColor(textColor);
-    g.drawString("YXY",(int)x,(int)y);
+    g.drawString("YXY",(int)(x-radius),(int)y);
+  }
 
+  public void keepOnBoard(){
+    if (this.x < this.radius) {
+      // it went off the left edge! do something!
+      this.vx = -this.vx;
+      this.x = this.radius;
+      this.textColor=new Color(102,178,255);
+
+
+    }else if (this.x > CircleShape.boardWidth-this.radius) {
+      // it went off the right edge! do something!
+      this.vx = -this.vx;
+      this.x = CircleShape.boardWidth-this.radius;
+      this.textColor=new Color(0,153,76);
+    }
+
+    if (this.y < this.radius){
+      // it went above the top edge!
+      this.vy = -this.vy;
+      this.y = this.radius;
+      this.textColor=new Color(255,102,255);
+
+    } else if (this.y > CircleShape.boardHeight-this.radius) {
+      // it went below the bottom edge!
+      this.vy = -this.vy;
+      this.y = CircleShape.boardHeight-this.radius;
+      this.textColor=new Color(204,102,0);
+    }
   }
 
   public String toString(){
